99cad248d92dd68344deec985e892aa30c1eeb04
Mahalakshmi025 <pmahalakshmi025@gmail.com>
2025-08-14 15:19:53 +0530

    arrays

--- 11-installations.sh
+++ 11-installations.sh
@@ -5,43 +5,41 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
 then
-   echo "Please run this script with root previlages"
+    echo "Please run this script with root priveleges"
+    exit 1
 fi
 
-dnf list installed git # just checking whether it is installed or not
+dnf list installed git # Just checking whether installed or not
 
 if [ $? -ne 0 ]
-then 
-    echo "Git is not installed, going to install it"
+then
+    echo "Git is not installed, going to install it.."
     dnf install git -y # here installing
     if [ $? -ne 0 ]
-    then 
-       echo "Git installation is not success...check it"
-       exit 1
+    then
+        echo "Git installation is not success...check it"
+        exit 1
     else
-       echo "Git installation is success"
-
+        echo "Git installation is success"
     fi
 else
-   echo "Git is already installed, nothing to do.."
+    echo "Git is already installed, nothing to do.."
 fi
 
-dnf list installed mysql
+dnf list installed mysql 
 
 if [ $? -ne 0 ]
-then 
-    echo "MYSQL is not installed, going to install it"
+then
+    echo "MySQL is not installed...going to install"
     dnf install mysql -y
     if [ $? -ne 0 ]
     then
-        echo "Mysql installation is not success..check it"
+        echo "MySQL installation is failure..please check"
         exit 1
     else
-       echo "Mysql installation is success"
-
+        echo "MySQL installation is success"
     fi
 else
-   echo "MYSQL is already installed, nothing to do.."
+    echo "MySQL is already installed..nothing to do"
 fi
-
     
