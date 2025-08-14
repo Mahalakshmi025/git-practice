febdf6f2bdbcd4a48c86bd84e94bd3237b8ecbbe
Mahalakshmi025 <pmahalakshmi025@gmail.com>
2025-08-14 14:21:09 +0530

    arrays

--- 06-arrays.sh
+++ 06-arrays.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-FRUITS = ("APPLE" "KIWI" "ORANGES")
+
+# index starts from 0, size is 3
+FRUITS=("APPLE" "KIWI" "ORANGE") #Array
 
 echo "First fruit is: ${FRUITS[0]}"
 echo "Second fruit is: ${FRUITS[1]}"
 echo "Third fruit is: ${FRUITS[2]}"
+
 echo "First fruit is: ${FRUITS[@]}"
