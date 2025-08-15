96b6dcf475df1ffce9f7171432e342b16b792208
Mahalakshmi025 <pmahalakshmi025@gmail.com>
2025-08-15 20:03:52 +0530

    sets

--- 17-set.sh
+++ 17-set.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 set -e #setting the automatic exit, if we get error
-echo "Hello World"
-echooo "Hello World"
-echo "Hellow World"
+
+failure() {
+    echo "Failed at: $1:$2"
+
+}
+
+trap 'failure "${LINEND}" "BASH_COMMAND" ' ERR #ERR is the error signal
+
+echo "Hello World success"
+echooo "Hello World failure"
+echo "Hello World after failure"
